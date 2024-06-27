1afd913d84de793c0474193f26ed9b26cb4ef3da
Konstantinos Lampropoulos <konstantinoslamprop@gmail.com>
2024-06-27 13:13:06 +0300

    histogram_segmentation_using_scikit_image.py

--- 023-histogram_segmentation_using_scikit_image.py
+++ 023-histogram_segmentation_using_scikit_image.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-__author__ = "Sreenivas Bhattiprolu"
-__license__ = "Feel free to copy, I appreciate if you acknowledge Python for Microscopists"
-
-# https://www.youtube.com/watch?v=kIVk0IhDMwY
-
 # Histogram based segmentation
 
 from skimage import io
