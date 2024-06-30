85e62766f281cc244972a9d168e1d52adfb5f8d4
Konstantinos Lampropoulos <konstantinoslamprop@gmail.com>
2024-06-30 23:53:46 +0300

    Object Detection using OpenCV.py

--- Object Detection using OpenCV.py
+++ Object Detection using OpenCV.py
@@ -1,32 +1,7 @@
-
-
-# pip install opencv-contrib-python # some people ask the difference between this and opencv-python
-                                    # and opencv-python contains the main packages wheras the other
-                                    # contains both main modules and contrib/extra modules
-# pip install cvlib # for object detection
-
-# # pip install gtts
-# # pip install playsound
-# use `pip3 install PyObjC` if you want playsound to run more efficiently.
-
 import cv2
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 from gtts import gTTS
-from playsound import playsound
-from food_facts import food_facts
-
-
-
-
-def speech(text):
-    print(text)
-    language = "en"
-    output = gTTS(text=text, lang=language, slow=False)
-
-    output.save("./sounds/output.mp3")
-    playsound("./sounds/output.mp3")
-
 
 video = cv2.VideoCapture(1)
 labels = []
@@ -61,13 +36,4 @@ for label in labels:
 
     i += 1
 
-speech(" ".join(new_sentence))
-speech("Here are the food facts i found for these items:")
-
-for label in labels:
-    try:
-        print(f"\n\t{label.title()}")
-        food_facts(label)
-
-    except:
-        print("No food facts for this item")
+print(" ".join(new_sentence))
